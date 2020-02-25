4056ee6ec2aed58cc2d5ec4e5f448843f9df8a70
Philip Smith <philip.smith.k@gmail.com>
2020-02-25 13:37:08 -0500

    enabled migrations

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,29 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _dbContext = new RestaurantDbContext();
+
+        //POST
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
+        {
+            if (ModelState.IsValid && restaurant != null)
+            {
+                _dbContext.Restaurants.Add(restaurant);
+                await _dbContext.SaveChangesAsync();
+                return Ok(); 
+            }
+            else
+                return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestaurantRater/Migrations/202002251651469_InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿namespace RestaurantRater.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Style = c.String(nullable: false),
+                        Rating = c.Double(nullable: false),
+                        DollarSigns = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Restaurants");
+        }
+    }
+}
